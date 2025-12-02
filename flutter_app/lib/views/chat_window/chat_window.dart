3ff40e1163ab73b5ef4632308d975ef0f98d30d9
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-12-02 09:43:42 -0400

    feat: Message Model. Removed Chat and Dependency on Chat Model

--- flutter_app/lib/views/chat_window/chat_window.dart
+++ flutter_app/lib/views/chat_window/chat_window.dart
@@ -1,12 +1,11 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app/services/hive.service.dart';
 import 'package:flutter_app/views/utils/text_field.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-import '../../models/chat.model.dart';
 import '../../models/message.model.dart';
 import '../utils/bubble/bubble.dart';
+import '../utils/view_config.dart';
 
 class ChatWindow extends StatefulWidget {
   ChatWindow({super.key});
@@ -20,7 +19,7 @@ class _ChatWindowState extends State<ChatWindow> {
 
   final ScrollController _scrollController = ScrollController();
 
-  final HiveService _hiveService = HiveService();
+  final MessageService _messageService = MessageService();
 
   List<Widget> _messages = [];
 
@@ -28,77 +27,122 @@ class _ChatWindowState extends State<ChatWindow> {
 
   String chatId = "";
 
-  void _chatScrollListener() {
+  int newMessageCount = 0;
+
+  void _checkBounds() {
+    if (!_scrollController.hasClients) return;
     final double max = _scrollController.position.maxScrollExtent;
     final double curr = _scrollController.position.pixels;
     // 50 as a factor after multiple testing with respect to layout orientation
     final bool shouldBottom = (max - curr) > 50;
     setState(() {
       _activeScrollList = max > 0 && shouldBottom;
+      if (!_activeScrollList) newMessageCount = 0;
     });
   }
 
   void _scrollToBottom() {
+    if (!_scrollController.hasClients) return;
     _scrollController.animateTo(
-      _scrollController.position.maxScrollExtent + 120,
+      // Tiny factor to avoid overflow
+      _scrollController.position.maxScrollExtent + 1,
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeOut,
     );
+    setState(() {
+      newMessageCount = 0;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(_chatScrollListener);
+    _scrollController.addListener(_checkBounds);
 
     _readChat();
+    _openSocket();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
+  }
+
+  void _openSocket() {
+    /// Listens to events in Messages
+    _messageService.messages.watch().listen((message) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!_activeScrollList) _scrollToBottom();
+        if (!(message.value as MessageModel).isSender) {
+          setState(() {
+            newMessageCount++;
+          });
+        }
+      });
+    });
   }
 
   void _readChat() {
-    chatId = _hiveService.getChatId();
-    _messages = _hiveService.getChat()!.messages.map((message) {
-      if (message.isSender) {
-        return SenderBubble(message: message.text, time: message.timestamp);
-      }
-      return ReceiverBubble(message: message.text, time: message.timestamp);
-    }).toList();
+    _messages = _messageService
+        .getAllChats()
+        .map((message) => _newMessage(message))
+        .toList();
+  }
+
+  Widget _newMessage(MessageModel message) {
+    if (message.isSender) {
+      return SenderBubble(message: message.text, time: message.createdAt);
+    }
+    return ReceiverBubble(message: message.text, time: message.createdAt);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // extendBodyBehindAppBar: true,
-      appBar: AppBar(), // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     List<ChatModel> cha = _hiveService.getAllChats();
-      //     print("object");
-      //   },
-      // ),
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  itemCount: _messages.length,
-                  controller: _scrollController,
-                  itemBuilder: (context, index) => _messages[index],
-                ),
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: ValueListenableBuilder<Box<MessageModel>>(
+                valueListenable: _messageService.messages.listenable(),
+                builder: (context, value, _) {
+                  if (value.values.toList().isEmpty) return _emptyChat();
+
+                  final messages = value.values.toList()
+                    ..sort((a, b) => a.createdAt.compareTo(b.createdAt));
+
+                  return ListView.builder(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    itemCount: messages.length,
+                    controller: _scrollController,
+                    itemBuilder: (context, index) =>
+                        _newMessage(messages[index]),
+                  );
+                },
               ),
-              TextInputField(onMessage: (text) => handleSend(text, true)),
-            ],
-          ),
-          Visibility(
-            visible: _activeScrollList,
-            child: Positioned(
-              bottom: 120,
-              right: 0,
-              child: _scrollBottomButton(),
             ),
+            TextInputField(onMessage: (text) => handleSend(text, true)),
+          ],
+        ),
+        Visibility(
+          visible: _activeScrollList,
+          child: Positioned(
+            bottom: 120,
+            right: 0,
+            child: _scrollBottomButton(),
           ),
-        ],
+        ),
+      ],
+    );
+  }
+
+  Widget _emptyChat() {
+    return Center(
+      child: RichText(
+        text: TextSpan(
+          text: "Hello!, Start a new conversation!",
+          style: kTextStyle(context).copyWith(fontSize: kTextLarge(context)),
+        ),
       ),
     );
   }
@@ -106,45 +150,49 @@ class _ChatWindowState extends State<ChatWindow> {
   Widget _scrollBottomButton() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: FloatingActionButton(
-        onPressed: () {
-          _scrollToBottom();
-        },
-        child: Icon(Icons.keyboard_arrow_down_rounded),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              _scrollToBottom();
+            },
+            child: Icon(Icons.keyboard_arrow_down_rounded),
+          ),
+          Positioned(
+            right: 0,
+            top: -10,
+            child: Visibility(
+              visible: newMessageCount > 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Text(
+                    "$newMessageCount",
+                    style: kTextStyle(context).copyWith(
+                      fontSize: kTextSmall(context),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   void handleSend(String text, bool isSender) {
-    _hiveService.addChat(
-      ChatModel(chatId: chatId, chatName: "New Chat", messages: []),
-    );
-
     if (isSender) {
       setState(() {
         _messages.add(SenderBubble(message: text, time: DateTime.now()));
       });
-
-      _hiveService
-          .addMessage(chatId, MessageModel(text: text, isSender: isSender))
-          .then((response) {
-            // a delay of 2 seconds to read
-            Timer(Duration(seconds: 2), () {
-              _hiveService.addReply(
-                chatId,
-                MessageModel(text: response, isSender: false),
-              );
-              setState(() {
-                _messages.add(
-                  ReceiverBubble(message: response, time: DateTime.now()),
-                );
-                if (_messages.length > 18) {
-                  _scrollToBottom();
-                }
-              });
-              _scrollToBottom();
-            });
-          });
+      _messageService.addMessage(MessageModel(text: text, isSender: isSender));
     }
   }
 }
